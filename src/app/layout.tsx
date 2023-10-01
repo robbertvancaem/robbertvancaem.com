956a1f9bcf243eb6b0438166e67923e5671e856c
Robbert van Caem <robbertvancaem@gmail.com>
2023-10-01 15:35:08 +0200

    Add Prettier and first sections of homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,41 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+import Link from "next/link";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'robbertvancaem.com',
-  description: 'Frontend developer',
-}
+  title: "robbertvancaem.com",
+  description: "Frontend developer",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="flex justify-between p-4 fixed backdrop-saturate-100 backdrop-blur w-full shadow-md">
+          <Link href="/">
+            <span className="font-extrabold">robbertvancaem</span>
+            <span className="font-light">.com</span>
+          </Link>
+          <div className="space-x-12">
+            <Link href="/about">About</Link>
+            <Link href="/projects">Projects</Link>
+            <Link
+              href="/contact"
+              className="bg-white text-slate-900 py-2 px-4 rounded-lg"
+            >
+              Contact
+            </Link>
+          </div>
+        </nav>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,126 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Image from "next/image";
 
-const listConfig: {emoji: string, text: string}[] = [{
-  emoji: '🎩',
-  text: 'I actually enjoy doing presentations'
-}, {
-  emoji: '💬',
-  text: "I'm not afraid of talking to clients directly"
-}, {
-  emoji: '🗒',
-  text: "I will create meaningful Jira tickets"
-}, {
-  emoji: '🤓',
-  text: "I don't mind doing backend work (some)"
-}, {
-  emoji: '💼',
-  text: "I'm all about SCRUM"
-}]
+const GRADIENT_CLASS =
+  "text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500";
+const LINK_CLASS = `border-b font-bold hover:text-pink-400 hover:border-pink-400`;
 
-const Li = ({ children } : { children: React.ReactNode}) => <li className="m-4 text-lg">{children}</li>
-const Emoji = ({ children } : { children: React.ReactNode}) => <div className="aspect-square bg-white/10 rounded-full text-2xl w-12 inline-flex items-center justify-center mr-4">{children}</div>
-const ListItem = ({ emoji, text }: { emoji: string, text: string }) => <Li key={emoji}><Emoji>{emoji}</Emoji>{text}</Li>
+const listConfig: { emoji: string; text: string }[] = [
+  {
+    emoji: "🎩",
+    text: "I actually enjoy doing presentations",
+  },
+  {
+    emoji: "💬",
+    text: "I'm not afraid of talking to clients directly",
+  },
+  {
+    emoji: "🗒",
+    text: "I will create meaningful Jira tickets",
+  },
+  {
+    emoji: "🤓",
+    text: "I don't mind doing backend work (some)",
+  },
+  {
+    emoji: "💼",
+    text: "I'm all about SCRUM",
+  },
+];
+
+const Section = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => <section className={`aspect-video ${className}`}>{children}</section>;
+
+const Li = ({ children }: { children: React.ReactNode }) => (
+  <li className="m-4 text-lg">{children}</li>
+);
+const Emoji = ({ children }: { children: React.ReactNode }) => (
+  <div className="aspect-square bg-white/10 rounded-full text-2xl w-12 inline-flex items-center justify-center mr-4">
+    {children}
+  </div>
+);
+const ListItem = ({ emoji, text }: { emoji: string; text: string }) => (
+  <Li key={emoji}>
+    <Emoji>{emoji}</Emoji>
+    {text}
+  </Li>
+);
 
 export default function Home() {
   return (
     <main>
-      <nav className="flex justify-between p-4 fixed backdrop-saturate-100 backdrop-blur w-full shadow-md">
-        <Link href="/">
-          <span className="font-extrabold">robbertvancaem</span>
-          <span className="font-light">.com</span>
-        </Link>
-        <div className="space-x-6">
-          <a href="#">About</a>
-          <a href="#">Projects</a>
-          <a href="#">Contact</a>
-        </div>
-      </nav>
-      <div className="flex items-center justify-center py-24">
+      <Section className="flex items-center justify-center py-24">
         <div className="w-48 m-2 overflow-hidden rounded-full">
-          <Image src="/me.jpeg" width={800} height={800} alt="Robbert van Caem" priority />
+          <Image
+            src="/me.jpeg"
+            width={800}
+            height={800}
+            alt="Robbert van Caem"
+            priority
+          />
         </div>
         <div className="m-4">
-          <h1 className="text-[3.375rem] font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">Robbert van Caem</h1>
-          <h2 className="text-4xl font-bold">Full Stack Developer</h2>
+          <h1 className={`text-[3.375rem] font-extrabold ${GRADIENT_CLASS}`}>
+            Robbert van Caem
+          </h1>
+          <h2 className={`text-3xl font-bold ${GRADIENT_CLASS}`}>
+            Creative Frontend Developer
+          </h2>
+          <h3 className="text-sm mt-8">
+            Experienced in{" "}
+            <a href="https://react.dev" className={LINK_CLASS}>
+              React
+            </a>
+            ,{" "}
+            <a href="https://nextjs.org/" className={LINK_CLASS}>
+              NextJS
+            </a>
+            ,{" "}
+            <a href="https://jestjs.io/" className={LINK_CLASS}>
+              Jest
+            </a>
+            , CI/CD and many more
+          </h3>
         </div>
-      </div>
-      <div className="py-4 px-16 border border-slate-800">
-        <h2 className="text-2xl font-bold">What makes me a &quot;modern&quot; developer?</h2>
-        <ul>
-          {listConfig.map(ListItem)}
-        </ul>
-      </div>
-      <div className="py-4 flex justify-center items-center border border-slate-800">Slide 2</div>
+      </Section>
+      <Section className="flex px-10">
+        <div className="w-1/2 flex justify-center">
+          <h2 className={`text-4xl font-bold ${GRADIENT_CLASS}`}>
+            Hi there 👋
+          </h2>
+        </div>
+        <div className="w-1/2 space-y-4">
+          <p>
+            I am a Creative Frontend Developer with more than 10 years of
+            experience. The past decade, I have been working for creatives
+            agencies as well as bigger, more corporate companies.
+          </p>
+        </div>
+      </Section>
+      <Section className="py-4 px-16 border border-slate-800">
+        <h2 className="text-2xl font-bold">
+          What makes me a &quot;modern&quot; developer?
+        </h2>
+        <ul>{listConfig.map(ListItem)}</ul>
+      </Section>
+      <Section className="py-4 flex justify-center items-center border border-slate-800">
+        Slide 2
+      </Section>
       <div className="py-4 flex justify-center items-center border border-slate-800">
-        <a href="https://github.com/robbertvancaem" target="_blank">Github</a>
-        <a href="https://www.linkedin.com/in/robbertvancaem/" target="_blank">LinkedIn</a>
-        <a href="https://robbertvancaem.medium.com/" target="_blank">Medium</a>
+        <a href="https://github.com/robbertvancaem" target="_blank">
+          Github
+        </a>
+        <a href="https://www.linkedin.com/in/robbertvancaem/" target="_blank">
+          LinkedIn
+        </a>
+        <a href="https://robbertvancaem.medium.com/" target="_blank">
+          Medium
+        </a>
       </div>
     </main>
-  )
+  );
 }
